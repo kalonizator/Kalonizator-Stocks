58e2aff1e0a5b6c970da641f2aaa04411fb4358d
kalonizator <halelov_adlet@mail.ru>
2022-06-30 18:00:06 +0600

    Initial Commit

---
+++ Kalonizator Stocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kalonizator Stocks
+//
+//  Created by Adilet on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
